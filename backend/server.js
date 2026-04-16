cfc52f677eb1e49ef6a2ef7dcf02908146bc5aec
amangupta2509 <amangupta.t6@gmail.com>
2026-04-16 17:26:54 +0530

    removed local uploads middleware

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/uploads", express.static("uploads"));
 
 // ===================================
 // MONGODB CONNECTION
